b86487d2b2c58c7c01158898db0f9cfb5324e52c
JOELwindows7 <JOELwindows7@users.noreply.github.com>
2026-08-20 14:14:49 +0700

    .

---
+++ landing/custom/chromiumExtension/README.md
@@ -0,0 +1,3 @@
+# Informal Chromium Extensions
+
+Chrome Extenstion that's not available on Chrome Web Store due to certain shenangicaanfjksdhaflkjhasdjkfhlsdujk
---
+++ landing/custom/chromiumExtension/updateOverwriteAdnauseam.ps1
@@ -0,0 +1,12 @@
+#!/bin/pwsh
+# Download new AdNauseam Chromium Extension ZIP & overwrite the old one
+
+$Wget = "wget"
+$7zip = "7z"
+$Download = "https://github.com/dhowe/AdNauseam/releases/latest/download/adnauseam.chromium.zip"
+
+Invoke-Expression "$($Wget) $($Download) -N"
+
+## then extract!
+
+Invoke-Expression "$(7zip) x adnauseam.chromium.zip"
